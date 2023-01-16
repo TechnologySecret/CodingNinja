3aeb2ed22c548caa3da323f0a735d65ecf587e77
Shailesh J <shailesh21319@gmail.com>
2023-01-16 15:33:50 +0530

    Create 5ReverseOfaNumber.java

---
+++ Milestone 1 Java/Lecture7/5ReverseOfaNumber.java
@@ -0,0 +1,42 @@
+/*
+Reverse of a number
+
+Write a program to generate the reverse of a given number N. Print the corresponding reverse number.
+Note : If a number has trailing zeros, then its reverse will not include them. For e.g., reverse of 10400 will be 401 instead of 00401.
+
+
+Input format :
+Integer N
+Output format :
+Corresponding reverse number
+Constraints:
+0 <= N < 10^8
+Sample Input 1 :
+1234
+Sample Output 1 :
+4321
+Sample Input 2 :
+1980
+Sample Output 2 :
+891
+*/
+
+import java.util.*;
+public class Main {
+	static int reverse(int n)
+    {
+        int revNum = 0;
+        while (n > 0) {
+            revNum = revNum * 10 + n % 10;
+            n = n / 10;
+        }
+        return revNum;
+    }
+	public static void main(String[] args) {
+		// Write your code here
+		Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(reverse(n));
+
+	}
+}

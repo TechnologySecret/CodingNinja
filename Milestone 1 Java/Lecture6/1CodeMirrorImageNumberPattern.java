89d45510afb4b845b0662546de0b5f1dcb1f2e28
Shailesh J <shailesh21319@gmail.com>
2023-01-16 15:05:18 +0530

    Create  1CodeMirrorImageNumberPattern.java

---
+++ Milestone 1 Java/Lecture6/1CodeMirrorImageNumberPattern.java
@@ -0,0 +1,61 @@
+/*
+Code : Mirror Image Number Pattern
+
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+
+The dots represent spaces.
+
+
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+3
+Sample Output 1:
+      1 
+    12
+  123
+Sample Input 2:
+4
+Sample Output 2:
+      1 
+    12
+  123
+1234
+
+*/
+
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+	 Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int i=1;
+        while(i<= n){
+            int star=1;
+            int j=1;
+            while(j<=n-i){
+                System.out.print(" ");
+                j++;
+            }
+            while(j<= n){
+                System.out.print(star);
+                star++;
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+		
+	}
+
+}

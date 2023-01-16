991f9cca05da1544d81cb4b8175c87a89bcf9a0a
Shailesh J <shailesh21319@gmail.com>
2023-01-16 15:29:21 +0530

    Create 2AllPrimeNumbers.java

---
+++ Milestone 1 Java/Lecture7/2AllPrimeNumbers.java
@@ -0,0 +1,54 @@
+/*
+All Prime Numbers
+Given an integer N, print all the prime numbers that lie in the range 2 to N (both inclusive).
+Print the prime numbers in different lines.
+Input Format :
+Integer N
+Output Format :
+Prime numbers in different lines
+Constraints :
+1 <= N <= 100
+Sample Input 1:
+9
+Sample Output 1:
+2
+3
+5
+7
+Sample Input 2:
+20
+Sample Output 2:
+2
+3
+5
+7
+11
+13
+17
+19
+*/
+
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 2; i <= n; i++) {
+			int count = 0;
+			for (int j = 2; j <= i; j++) {
+				if (i % j == 0)
+					count++;
+			}
+			if (count == 1)
+				System.out.println(i);
+		}
+		
+	}
+}
+

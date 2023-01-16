5bd6caa9d4c8d2621db53423eacb96857597ab76
Shailesh J <shailesh21319@gmail.com>
2023-01-17 02:22:03 +0530

    Create 3SortLL.java

---
+++ Milestone 1 DSA/Test5/3SortLL.java
@@ -0,0 +1,17 @@
+/*
+Sort Linked List
+Send Feedback
+Given a Linked List, which has nodes in alternating ascending and descending orders. Sort the list efficiently and space complexity should be O(1).
+You just need to return the head of sorted linked list, don't print the elements.
+Input format :
+Line 1 : Linked list elements of length L (separated by space and terminated by -1)
+Line 2 : Integer n
+Output format :
+Updated list elements (separated by space)
+Sample Input 1 :
+10 40 53 30 67 12 89 -1
+Sample Output 1 :
+10 12 30 40 53 67 89
+*/
+
+Answers is not available 

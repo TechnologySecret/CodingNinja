99bc578f2e2b4dbb0ccaff7007301972d56663d5
Shailesh J <shailesh21319@gmail.com>
2023-01-16 22:35:27 +0530

    Update 3FaultyKeyboard.java

--- Milestone 1 Java/Test4/3FaultyKeyboard.java
+++ Milestone 1 Java/Test4/3FaultyKeyboard.java
@@ -24,3 +24,45 @@ Sample Output 1
 true
 false
 */
+
+import java.util.*;
+public class Solution{
+  public static boolean isPossible(String name, String typed) {
+    // Write your code here
+    if (name.length() > typed.length())
+      return false;
+    int i = 0;
+    int j = 0;
+    while (i != name.length() && j != typed.length()) {
+      if (name.charAt(i) == typed.charAt(j)) {
+        i++;
+        j++;
+      } else if (i > 0 && name.charAt(i - 1) == typed.charAt(j)) {
+        j++;
+      } else {
+        return false;
+      }
+    }
+
+    while (j < typed.length()) {
+      if (typed.charAt(j) != name.charAt(i - 1))
+        return false;
+      j++;
+    }
+
+    return i < name.length() ? false : true;
+
+  }
+
+    public static void main(String[] args) {
+		// Write your code here
+    Scanner scn = new Scanner (System.in);
+    int n = scn.nextInt();
+    for(int i = 0; i<n; i++){
+      String name= scn.next();
+      String typed= scn.next();
+      boolean  res = isPossible(name, typed);
+      System.out.println(res);
+    }
+    }
+}

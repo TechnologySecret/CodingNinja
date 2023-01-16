8c91794d00be00c59201e6137796de949b8631d2
Shailesh J <shailesh21319@gmail.com>
2023-01-17 00:27:31 +0530

    Update 8CheckAB.java

--- Milestone 1 DSA/AssignmentRecu/8CheckAB.java
+++ Milestone 1 DSA/AssignmentRecu/8CheckAB.java
@@ -24,3 +24,23 @@ Explanation for Sample Input 2
 In the above example, a is not followed by either "a" or "bb", instead it's followed by "b" which results in false to be returned.
 */
 
+public class Solution {
+
+	public static boolean checkAB(String input) {
+		// Write your code here
+	if(input.length()==0){
+		return true;
+	}
+	if(input.charAt(0) == 'a'){
+          if(input.substring(1).length() > 1 && input.substring(1,3).equals("bb")){
+              return checkAB(input.substring(3));
+          }else{
+               return checkAB(input.substring(1));
+          }
+      }
+        return false;
+
+	}
+}
+
+

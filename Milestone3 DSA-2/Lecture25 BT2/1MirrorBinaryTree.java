ab43510e156b9791785318012915528d3d7c4a94
Shailesh J <shailesh21319@gmail.com>
2023-01-17 11:48:31 +0530

    Update 1MirrorBinaryTree.java

--- Milestone3 DSA-2/Lecture25 BT2/1MirrorBinaryTree.java
+++ Milestone3 DSA-2/Lecture25 BT2/1MirrorBinaryTree.java
@@ -31,3 +31,37 @@ Sample Output 2:
 3 2 
 9
 */
+
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static void mirrorBinaryTree(BinaryTreeNode<Integer> root){
+		//Your code goes here
+		if(root ==null){
+			return;
+		}
+		BinaryTreeNode<Integer> temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+		mirrorBinaryTree(root.left);
+		mirrorBinaryTree(root.right);
+	}
+	
+}
